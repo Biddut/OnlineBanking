97c2806cfbb7ea26e44a4847d6031045acabbe31
biddut.bhowmic <biddut.bhowmic@gmail.com>
2020-06-02 02:43:59 -0400

    Front End login and signup page

---
+++ src/test/java/com/javabank/online/CustomerPortalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javabank.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Spring boot working" );
+	}
+
+}
